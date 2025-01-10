5089fb89e0b4bb1b01fd1ce0ee65c75af1955c27
Sushant <sushant889427@gmail.com>
2025-01-10 23:45:02 +0530

    Final Build

--- frontend/src/controllers/delete_cluster.js
+++ frontend/src/controllers/delete_cluster.js
@@ -3,10 +3,10 @@ import axios from "axios";
 export async function deleteCluster(id) {
   try {
     // Send the DELETE request to the backend API to delete the cluster
-    console.log("===================> id = ", id)
-    const response = await axios.delete(`http://localhost:8000/cluster/delete/${id}`);
+    // console.log("===================> id = ", id)
+    // const response = await axios.delete(`http://localhost:8000/cluster/delete/${id}`);
 
-    // const response = await axios.delete(`https://geo-cluster-analytics-production.up.railway.app/cluster/delete/${id}`);
+    const response = await axios.delete(`https://geo-cluster-analytics-production.up.railway.app/cluster/delete/${id}`);
 
     // If the deletion is successful, handle the response here
     console.log("Cluster deleted successfully:", response.data.message);
